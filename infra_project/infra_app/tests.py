bf4a18c1933627ff93280b15c58d633a18d2d1d4
andrew_cherry <chernyaevav@gmail.com>
2023-02-04 20:15:17 +0300

    первое изменение в коде (исправил тесты)

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -3,7 +3,6 @@ from http import HTTPStatus
 from django.test import Client, TestCase
 
 
-
 class StaticPagesURLTests(TestCase):
     def setUp(self):
         self.guest_client = Client()
@@ -18,10 +17,5 @@ class StaticPagesURLTests(TestCase):
 
     def test_page_shows_correct_content(self):
         """Проверка контента страниц."""
-        # response = self.guest_client.get('/')
-        # self.assertContains(response,
-        #                     ('У меня получилось. Запустил приложение в докере'
-        #                         'на удаленном сервере!'))
-
         response = self.guest_client.get('/second_page/')
         self.assertContains(response, 'А это вторая страница!')
