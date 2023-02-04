0ed3b9fba2d1ef504b7c4e846471a471f3062b09
andrew_cherry <chernyaevav@gmail.com>
2023-02-04 20:07:36 +0300

    первое изменение в коде

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось. Запустил приложение в докере'
+                        'на удаленном сервере!')
 
 
 def second_page(request):
